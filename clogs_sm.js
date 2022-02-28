38b08fd689429ad044c2444d423076dd05972c9b
CrisMod <78448522+CrisMod@users.noreply.github.com>
2022-02-27 23:01:28 -0500

    Add files via upload

---
+++ clogs_sm.js
@@ -0,0 +1,37 @@
+{
+	"packages": [
+		{	
+			"version": "7.00",
+			"package": "com.whatsapp",
+			"status": "update_title",
+			"title": "WhatsApp {SM} V7.00",
+			"note": "false",
+			"msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsAppSuperMini/main/dark.html",
+			"download": "http://fumacrom.com/3OOJ4",
+			"update": [
+				{
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsAppSuperMini/main/changelog-d.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsAppSuperMini/main/changelog-d.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsAppSuperMini/main/changelog-l.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsAppSuperMini/main/changelog-l.html"
+				}
+			]
+		}
+	]
+}
+    ]
+}
